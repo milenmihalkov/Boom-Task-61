083505e5df6b710b5da95b3a8628f4f159f8e413
Milen <milenmihalkov@gmail.com>
2023-01-11 16:50:07 +0200

    emo

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  let hotprice = document.getElementsByClassName('hot');
+  let hotprice = document.querySelectorAll('p.price.hot');
   const fireEmoji='&#128293;';
 
   for(let i=0; i<hotprice.length; i++){
