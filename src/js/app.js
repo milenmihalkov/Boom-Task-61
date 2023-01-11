74595ca6a008c9b94edc29d7aeee55a75a9ab8f7
Milen <milenmihalkov@gmail.com>
2023-01-11 16:39:06 +0200

    Fire Emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let hotprice = document.getElementsByClassName('hot');
+  const fireEmoji='&#128293;';
+
+  for(let i=0; i<hotprice.length; i++){
+    hotprice[0].innerHTML+=fireEmoji;
+  }
+
 });
